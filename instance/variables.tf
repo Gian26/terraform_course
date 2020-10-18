fd7095334c07d1f688fd35a7e2ca50aabfc87ee9
Gian Cuello <giancuello26@gmail.com>
2020-10-18 17:10:26 -0500

    first push

---
+++ instance/variables.tf
@@ -0,0 +1,20 @@
+variable "ami_id"{
+    default="ami-06e54d05255faf8f6"
+    description="its the os being installed in the vm"
+}
+
+variable "instance_type"{
+
+}
+
+variable "tags"{
+
+}
+
+variable "sg_name"{
+
+}
+
+variable "ingress_rules"{
+
+}
